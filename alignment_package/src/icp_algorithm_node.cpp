154eb122c58ce1768266d32bb78ae7d4397bc67a
y4semin <yaseminakin415@gmail.com>
2025-02-12 16:53:21 +0300

    file git commit -m file

--- alignment_package/src/icp_algorithm_node.cpp
+++ alignment_package/src/icp_algorithm_node.cpp
@@ -5,13 +5,7 @@ int main(int argc, char** argv)
 {
     init(argc, argv, "alignment_package");
     NodeHandle nh;
-
-    aligned_pub = nh.advertise<sensor_msgs::PointCloud2>("/aligned_cloud", 10);
-
-    Subscriber source_sub = nh.subscribe("/cloud_all_fields_fullframe", 10, sourcecallback);
-    Subscriber target_sub = nh.subscribe("/velodyne_points",10, targetcallback);
+    
     spin();
-    icp_alignment();  
-
     return 0;
 }
