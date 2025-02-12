b28176d68a10ccc07e82b8ef1bfb037fdedf16ad
y4semin <yaseminakin415@gmail.com>
2025-02-12 16:51:07 +0300

    file

--- alignment_package/src/icp_algorithm.cpp
+++ alignment_package/src/icp_algorithm.cpp
@@ -1,5 +1,6 @@
 
 #include "alignment_package/AlignmentPackage.hpp"
+#include <string>
 
 Subscriber source_sub = nh.subscribe("/cloud_all_fields_fullframe", 10, sourcecallback);
 Subscriber target_sub = nh.subscribe("/velodyne_points",10, targetcallback);
