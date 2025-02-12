58791ae5aaaab4440f5b32633bddcfbb933de11e
y4semin <yaseminakin415@gmail.com>
2025-02-12 18:38:56 +0300

    file

--- alignment_package/include/AlignmentPackage.hpp
+++ alignment_package/include/AlignmentPackage.hpp
@@ -1,8 +1,43 @@
 #include <ros/ros.h>
 #include <sensor_msgs/PointCloud2.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/point_types.h>
+#include <pcl/registration/icp.h>
+#include <iostream>
 
-void sourcecallback (const sensor_msgs::PointCloud2ConstPtr& lidar_data);
+void sourcecallback (const sensor_msgs::PointCloud2ConstPtr& lidar_data)
+{
+    ros::fromROSMsg(*lidar_data, *source_cloud);  
+}
 
-void targetcallback (const sensor_msgs::PointCloud2ConstPtr& lidar_data);
 
-void icp_alignment();
+void targetcallback (const sensor_msgs::PointCloud2ConstPtr& lidar_data)
+{  
+    ros::fromROSMsg(*lidar_data, *target_cloud);    
+}
+
+
+void icp_alignment()
+{
+    pcl::IterativeClosestPoint<PointXYZ , PointXYZ> icp;
+    icp.setInputSource(source_cloud); 
+    icp.setInputTarget(target_cloud);   
+    pcl::PointCloud<PointXYZ> aligned_cloud;
+    icp.align(aligned_cloud);
+
+    sensor_msgs::PointCloud2 output_cloud;
+    ros::toROSMsg(aligned_cloud, output_cloud);
+    output_cloud.header.frame_id = "map";  
+    aligned_pub.publish(output_cloud); 
+
+
+    Eigen::Matrix4d matrix = icp.getFinalTransformation ().cast<double>();
+   
+    ros::ROS_INFO("Rotation matrix :");
+    ros::ROS_INFO("    | %f %f %f |", matrix (0, 0), matrix (0, 1), matrix (0, 2));
+    ros::ROS_INFO("R = | %f %f %f |", matrix (1, 0), matrix (1, 1), matrix (1, 2));
+    ros::ROS_INFO("    | %f %f %f |", matrix (2, 0), matrix (2, 1), matrix (2, 2));
+
+    ros::ROS_INFO("Translation vector :");
+    ros::ROS_INFO("t = %f, %f, %f", matrix (0, 3), matrix (1, 3), matrix (2, 3));   
+}  
--- alignment_package/src/icp_algorithm.cpp
+++ alignment_package/src/icp_algorithm.cpp
@@ -1,5 +1,8 @@
 #include "alignment_package/AlignmentPackage.hpp"
 #include <string>
 
+pcl::PointCloud<PointXYZ>::Ptr source_cloud (new pcl::PointCloud<PointXYZ>);
+pcl::PointCloud<PointXYZ>::Ptr target_cloud (new pcl::PointCloud<PointXYZ>);
+
 Subscriber source_sub = nh.subscribe("/cloud_all_fields_fullframe", 10, sourcecallback);
 Subscriber target_sub = nh.subscribe("/velodyne_points",10, targetcallback);
